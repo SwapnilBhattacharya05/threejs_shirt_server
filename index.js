5431905de20472d6c1ba00f4e754626cc8293983
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-01-25 00:57:45 +0530

    setup the backend server for ai image generation

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import dalleRoutes from './routes/dalle.routes.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+// CONSUME THE ROUTES AS MIDDLEWARE
+app.use('/api/v1/dalle', dalleRoutes);
+
+
+app.get('/', (req, res) => {
+    res.status(200).json('Hello from DALL-E AI')
+})
+
+app.listen(8080, () => {
+    console.log(`Server listening on port 8080`);
+})
